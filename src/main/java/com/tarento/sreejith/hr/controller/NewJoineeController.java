377511f2dcd6e7951934c9a6b238e2b0a68c4a41
Sreejith <jithupreethy@gmail.com>
2023-12-16 12:47:21 +0530

    updated endpoint to get all data for new joinees

--- src/main/java/com/tarento/sreejith/hr/controller/NewJoineeController.java
+++ src/main/java/com/tarento/sreejith/hr/controller/NewJoineeController.java
@@ -1,12 +1,23 @@
 package com.tarento.sreejith.hr.controller;
 
+import com.tarento.sreejith.hr.model.NewJoinee;
+import com.tarento.sreejith.hr.service.NewJoineeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/newjoinee")
 public class NewJoineeController {
+
+    @Autowired
+    NewJoineeService newJoineeService;
+
     @GetMapping("/")
-    public String testGetEndPoint() {
-        return "End point active";
+    public List<NewJoinee> getAllNewJoinee() {
+        return newJoineeService.readAllNewJoinee();
     }
 }
