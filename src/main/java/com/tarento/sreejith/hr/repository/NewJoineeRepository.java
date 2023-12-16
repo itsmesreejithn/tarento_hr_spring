8d5336b7d64c4de66556004f53e9f3f779fa2a2b
Sreejith <jithupreethy@gmail.com>
2023-12-16 12:36:39 +0530

    added repository annotation

--- src/main/java/com/tarento/sreejith/hr/repository/NewJoineeRepository.java
+++ src/main/java/com/tarento/sreejith/hr/repository/NewJoineeRepository.java
@@ -2,6 +2,8 @@ package com.tarento.sreejith.hr.repository;
 
 import com.tarento.sreejith.hr.model.NewJoinee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NewJoineeRepository extends JpaRepository<NewJoinee, Integer> {
 }
