121432dacf99bebbf36dd6a82d51cbdf39b87261
Sreejith <jithupreethy@gmail.com>
2023-12-16 12:39:58 +0530

    added service to find all new joinee

--- src/main/java/com/tarento/sreejith/hr/service/NewJoineeService.java
+++ src/main/java/com/tarento/sreejith/hr/service/NewJoineeService.java
@@ -1,12 +1,20 @@
 package com.tarento.sreejith.hr.service;
 
+import com.tarento.sreejith.hr.model.NewJoinee;
 import com.tarento.sreejith.hr.repository.NewJoineeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class NewJoineeService {
 
     @Autowired
     NewJoineeRepository newJoineeRepository;
 
+    public List<NewJoinee> readAllNewJoinee() {
+        return newJoineeRepository.findAll();
+    }
 
 }
